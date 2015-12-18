0e7082e71208a0ad5d982c20402628568b57f7ed
Marcos Manuel Ortega <info@indavelopers.com>
2015-12-18 20:14:45 +0100

    Bootstrap added

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# -*- coding: utf-8 -*-
+
+# Project: "EmprendeNet"
+# Script: main.py - App handler mapping
+# App identifier: emprende-net
+# URL: www.---.com
+# Author: Marcos Manuel Ortega - Indavelopers
+# Version: v1.0 - 12/2015
+
+
+# -- Imports --
 import webapp2
 
+from scripts.handlers import *
+
+
+# -- URL mapping --
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+
+# -- App --
+app = webapp2.WSGIApplication([('/', StaticPage),
+                               ('/contacto', StaticPage),
+                               ('/aviso-legal', StaticPage),
+                               ('/mapa-web', StaticPage),
+                               ('/webmap', Webmap)],
+                              debug=True)
